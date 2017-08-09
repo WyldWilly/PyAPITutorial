e7a365632c17b3e7a6dcc015129de5ec0692b1cc
Bill Costantini <costantiniwm@upmc.edu>
2017-08-09 12:24:24 -0400

    t

--- NumberAstros.py
+++ NumberAstros.py
@@ -10,15 +10,17 @@ data = response.json()
 tex = "There are "
 after = " astronauts in space currently"
 print(data["number"])
+print("1 -------")
 print(tex, data["number"], after)
 print(data)
 for name in data:
     print(data[name])
 
+print("2 -------")
 response = requests.get("http://api.open-notify.org/astros.json")
 print(response.content.decode("utf-8"))
 
-response = requests.get("http://api.open-notify.org/astros.json")
-people = {response}
-for key, val in people.items():
-    print(key, "=>", val)
+#response = requests.get("http://api.open-notify.org/astros.json")
+#people = {response}
+#for key, val in people.items():
+#    print(key, "=>", val)
